b865bf46a1be294e1a6a0f87e1234e0116a168a9
Jonas Noori <jonasgeneralassembly@gmail.com>
2021-09-18 12:02:46 +0100

    created properties anf prop children to be used in componenet

--- src/index.js
+++ src/index.js
@@ -2,28 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
+      <ApprovalCard>
       <CommentDetail
       author="Mole"
       date="Today at 10 :43AM"
       text="Hello"
       pic={faker.image.avatar()}
       />
+      </ApprovalCard>
+
+      <ApprovalCard>
       <CommentDetail
       author="Chubby"
       date="Today at 10 :43AM"
       text="Bonjour"
       pic={faker.image.avatar()}
       />
+      </ApprovalCard>
+
+      <ApprovalCard>
       <CommentDetail
       author="Snugry"
       date="Today at 10 :43AM"
       text="Asalami"
       pic={faker.image.avatar()}
       />
+      </ApprovalCard>
     </div>
   );
 };
