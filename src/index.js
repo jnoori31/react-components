e7edc4936ddb9475d5f907fe67b261940fdad0a3
Jonas Noori <jonasgeneralassembly@gmail.com>
2021-09-18 11:14:09 +0100

    1st componenet created with props

--- src/index.js
+++ src/index.js
@@ -1,52 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Jonas
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Jonas
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Jonas
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
+      <CommentDetail
+      author="Mole"
+      date="Today at 10 :43AM"
+      text="Hello"
+      pic={faker.image.avatar()}
+      />
+      <CommentDetail
+      author="Chubby"
+      date="Today at 10 :43AM"
+      text="Bonjour"
+      pic={faker.image.avatar()}
+      />
+      <CommentDetail
+      author="Snugry"
+      date="Today at 10 :43AM"
+      text="Asalami"
+      pic={faker.image.avatar()}
+      />
     </div>
   );
 };
