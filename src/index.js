283de9ae6a59bfe9fe6b420b98bcf7f5d22f1462
Jonas Noori <jonasgeneralassembly@gmail.com>
2021-10-11 18:35:12 +0100

    challenge complete

--- src/index.js
+++ src/index.js
@@ -8,31 +8,38 @@ const App = () => {
   return (
     <div className="ui container comments">
       <ApprovalCard>
-      <CommentDetail
-      author="Mole"
-      date="Today at 10 :43AM"
-      text="Hello"
-      pic={faker.image.avatar()}
-      />
+      <div>
+      <h4>Warning!</h4>
+      Are you sure you want to thub this?
+      </div>
       </ApprovalCard>
 
       <ApprovalCard>
-      <CommentDetail
-      author="Chubby"
-      date="Today at 10 :43AM"
-      text="Bonjour"
-      pic={faker.image.avatar()}
-      />
+        <CommentDetail
+        author="Mole"
+        date="Today at 10 :43AM"
+        text="Hello"
+        pic={faker.image.avatar()}
+        />
       </ApprovalCard>
 
       <ApprovalCard>
-      <CommentDetail
-      author="Snugry"
-      date="Today at 10 :43AM"
-      text="Asalami"
-      pic={faker.image.avatar()}
-      />
+        <CommentDetail
+        author="Chubby"
+        date="Today at 10 :43AM"
+        text="Bonjour"
+        pic={faker.image.avatar()}
+        />
       </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+        author="Snugry"
+        date="Today at 10 :43AM"
+        text="Asalami"
+        pic={faker.image.avatar()}
+        />
+        </ApprovalCard>
     </div>
   );
 };
